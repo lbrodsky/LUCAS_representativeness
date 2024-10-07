66994b2bc07944b4ad491fea5c151fdcc8b4af60
Martin Landa <landa.martin@gmail.com>
2024-10-07 13:55:37 +0200

    shp_generalize_max_ratio -> shp_generalize_min_ratio

--- region_grow/data_utils.py
+++ region_grow/data_utils.py
@@ -183,7 +183,7 @@ def convert_polygons2multi(layer):
 
 
 def vectorize_grown_point(grown_point, out_rg_layer, geo_transform, geo_proj, output_fields, lucas_geometry,
-                          shp_generalize_dist, shp_generalize_max_ratio=0.2,
+                          shp_generalize_dist, shp_generalize_min_ratio=0.2,
                           urban=False):
     """Convert the NumPy grown region into vector.
 
@@ -270,7 +270,7 @@ def vectorize_grown_point(grown_point, out_rg_layer, geo_transform, geo_proj, ou
 
     # perform shape generalization
     output_type = '_'.join(out_rg_layer.GetName().split('_')[-2:])
-    if shp_generalize_dist > 0 and output_fields["ratio"] > shp_generalize_max_ratio:
+    if shp_generalize_dist > 0 and output_fields["ratio"] > shp_generalize_min_ratio:
         logging.debug(f"Performing shape generalization (output={output_type})")
         # rg_geometry = rg_geometry.Buffer(-6, options=["JOIN_STYLE=MITRE"]).Buffer(6)
         rg_geometry = rg_geometry.Buffer(-shp_generalize_dist).Buffer(shp_generalize_dist-0.1)
