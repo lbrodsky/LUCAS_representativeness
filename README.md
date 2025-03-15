4e53b52f04e0c5bd0f614f094009f15b0cbc2ac6
Martin Landa <landa.martin@gmail.com>
2025-03-15 20:23:11 +0100

    extract lucas points: two gpkg

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Extract LUCAS points for 2018:
 wget https://geoforall.fsv.cvut.cz/extracts/st_lucas/db_st_lucas_dump.sql.7z -P data
 docker pull postgis/postgis:16-3.4
 docker run --name lucas-postgis -v ./data:/data -v `pwd`/utils:/opt/utils -e POSTGRES_PASSWORD=lucas -d postgis/postgis:16-3.4
-docker exec -ti lucas-postgis bash -c "apt-get update; apt-get -y install p7zip gdal-bin"
+docker exec -ti lucas-postgis bash -c "apt-get update; apt-get -y install p7zip-full gdal-bin"
 docker exec -ti lucas-postgis bash /opt/utils/extract_lucas_points.sh 2018
 docker stop lucas-postgis
 docker rm lucas-postgis
--- utils/extract_lucas_points.sh
+++ utils/extract_lucas_points.sh
@@ -12,11 +12,11 @@ DB=lucas
 createdb -U postgres $DB
 7z e -so ${DATA_DIR}/db_st_lucas_dump.sql.7z | sed '/^GRANT /d; /^ALTER DEFAULT PRIVILEGES /d' | psql -U postgres -d $DB
 
-filename=/data/eu_lucas_points_${YEAR}.gpkg
-rm -f $filename
-ogr2ogr -f GPKG $filename -overwrite PG:"dbname=lucas user=postgres" -nln eu_lucas_points_gps \
-	-sql "select point_id,geom_gps as geom,lc1 from data.lucas_points where geom_gps is not NULL and survey_year = ${YEAR}"
-ogr2ogr -f GPKG $filename -append PG:"dbname=lucas user=postgres" -nln eu_lucas_points_thr \
-	-sql "select point_id,geom_thr as geom,lc1 from data.lucas_points where geom_thr is not NULL and survey_year = ${YEAR}"
+filename=/data/eu_lucas_points
+rm -f ${filename}_*.gpkg
+ogr2ogr -f GPKG ${filename}_gps.gpkg -overwrite PG:"dbname=lucas user=postgres" -nln eu_lucas_points_gps \
+	-sql "select point_id,geom_gps as geom,lc1_h,gps_prec,obs_dist,obs_type,obs_direct from data.lucas_points where geom_gps is not NULL and survey_year = ${YEAR}"
+ogr2ogr -f GPKG ${filename}_thr.gpkg PG:"dbname=lucas user=postgres" -nln eu_lucas_points_thr \
+	-sql "select point_id,geom_thr as geom,lc1_h,gps_prec,obs_dist,obs_type,obs_direct from data.lucas_points where geom_thr is not NULL and survey_year = ${YEAR}"
 
 exit 0
