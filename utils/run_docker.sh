f67db8216a037934230ae96450984fc52aaaec92
Martin Landa <landa.martin@gmail.com>
2025-03-26 08:43:11 +0100

    run_docker: enable debug log

--- utils/run_docker.sh
+++ utils/run_docker.sh
@@ -30,7 +30,8 @@ for dir in `ls ${data_dir}/osm_clcplus/2018/*osm_clcplus* -d`; do
            --lucas_points /data/lucas_points/${country}_lucas_points_gps_${year}.gpkg \
            --lucas_thr_points /data/lucas_points/eu_lucas_points_thr_${year}.gpkg \
            --dst_dir /data/rg_repre_areas/${year}/${country}_rg_repre_areas \
-           --version ${version}
+           --version ${version} \
+	   --log_level debug
     break
 done
 
