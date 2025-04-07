58dc97867c59979856991ad7f3bed15ec8a022d0
Martin Landa <landa.martin@gmail.com>
2025-04-07 12:22:59 +0200

    latest_version: skip files

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,11 @@
 def latest_version(dirs):
     for cntr in dirs:
+        if not cntr.is_dir():
+            continue
         versions = cntr.glob('v*')
         v_max = 0
         for v in versions:
             v_num = int(v.name[1:])
-            if v_num > v_max: v_max = v_num
+            v_max = max(v_num, v_max)
 
     return v_max
