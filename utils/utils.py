de884fd57cfd2913e8102dc2f1c155441db35fe9
Martin Landa <landa.martin@gmail.com>
2025-04-10 17:19:45 +0200

    fix utils/utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import os
+import logging
+
 def latest_version(dirs):
     for cntr in dirs:
         if not cntr.is_dir():
@@ -9,3 +12,34 @@ def latest_version(dirs):
             v_max = max(v_num, v_max)
 
     return v_max
+
+country_codes = {
+    "austria": "AT",
+    "belgium": "BE",
+    "bulgaria": "BG",
+    "croatia": "HR",
+    "cyprus": "CY",
+    "czech-republic": "CZ",
+    "denmark": "DK",
+    "estonia": "EE",
+    "finland": "FI",
+    "france": "FR",
+    "germany": "DE",
+    "great-britain": "UK",
+    "greece": "EL",
+    "hungary": "HU",
+    "ireland": "IE",
+    "italy": "IT",
+    "latvia": "LV",
+    "lithuania": "LT",
+    "luxembourg": "LU",
+    "malta": "MT",
+    "netherlands": "NL",
+    "poland": "PL",
+    "portugal": "PT",
+    "romania": "RO",
+    "slovakia": "SK",
+    "slovenia": "SI",
+    "spain": "ES",
+    "sweden": "SE"
+}
